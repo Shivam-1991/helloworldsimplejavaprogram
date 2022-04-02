2cd8838699929356c0962ffbda453358cac79dc3
Shivam-1991 <102688826+Shivam-1991@users.noreply.github.com>
2022-04-02 23:19:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch25 team");
-      System.out.println("Hello This my first java prog");
+      System.out.println("Hello This my first java prog using automation");
     }  
 } 
